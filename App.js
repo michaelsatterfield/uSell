fa4822d71642fd56b1e02b851615121536e357fe
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-01 11:04:33 -0600

    reap

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
     StyleSheet,
     Text,
     View,
+    Image
 
 } from "react-native";
 
@@ -10,6 +11,7 @@ import {
 
 import Card from "./components/Card";
 import AnimatedButton from "./components/AnimatedButton";
+// import {Image} from "react-native-web";
 
 export default function App() {
 
@@ -20,15 +22,35 @@ export default function App() {
   return (
       <View
         style={{
-          backgroundColor: "#f8f4f4",
-          padding: 20,
-          paddingTop: 100
+          // backgroundColor: "#f8f4f4",
+          // padding: 20,
+          // paddingTop: 100
+             alignItems: 'center',
+              justifyContent: 'center',
+            flex:1
         }}>
-      <Card
-          image={require("./app/assets/jacket.jpg")}
-          subTitle="$100"
-          title="Red jacket for sale"
-      />
+
+
+          <Image
+          style={{
+
+
+              height: 200,
+              width: 300
+
+
+
+          }}
+          source={require('./app/assets/NewREAPLogo1-26-17.png')}
+          />
+
+
+
+      {/*<Card*/}
+      {/*    image={require("./app/assets/jacket.jpg")}*/}
+      {/*    subTitle="$100"*/}
+      {/*    title="Red jacket for sale"*/}
+      {/*/>*/}
       </View>
 
   );
