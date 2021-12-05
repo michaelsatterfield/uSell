9d622c61e35f68a1c03d190f2c948a6b15fed2b3
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-05 11:38:53 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	App.js

--- components/AnimatedButton.js
+++ components/AnimatedButton.js
@@ -13,7 +13,6 @@ const AnimatedButton = () => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
   const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
     Animated.timing(fadeAnim, {
       toValue: 1,
       duration: 5000,
