24f8aad4a52c07bcdbd05cb6a4d615776d5adfd3
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-07 16:01:43 -0600

    new screen

--- screens folder/ListingDetailsScreen.js
+++ screens folder/ListingDetailsScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Image, View, StyleSheet} from "react-native-web";
 
 const ListingDetailsScreen = () => {
     return (
         <View>
-
+    <Image  style={styles.image} source={require('../assets/jacket.jpg')}/>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    }
+})
+
 
 export default ListingDetailsScreen;
