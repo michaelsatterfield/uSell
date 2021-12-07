0745d1dab54494f2f31ce06a9c7623af2bb8da1f
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-07 16:18:57 -0600

    new screen

--- screens folder/ListingDetailsScreen.js
+++ screens folder/ListingDetailsScreen.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import {Image, View, StyleSheet} from "react-native-web";
+import {Image, View, StyleSheet}  from 'react-native';
+import AppText from "../components/AppText";
 
 const ListingDetailsScreen = () => {
     return (
         <View>
-    <Image  style={styles.image} source={require('../assets/jacket.jpg')}/>
+    <Image style={styles.image} source={require('../app/assets/jacket.jpg')}/>
+            <View style={styles.detailsContainer}>
+            <AppText style={styles.tile}>Red jacket for sale</AppText>
+            <AppText>$100</AppText>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    detailsContainer: {
+        padding: 20,
+    },
     image: {
         width: '100%',
         height: 300
+    },
+    title: {
+
     }
 })
 
