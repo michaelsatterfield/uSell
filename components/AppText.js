53e46a0f772e2dd6adbb3225eb65077e518a13dc
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-13 20:10:53 -0600

    Merge remote-tracking branch 'origin/master'

--- components/AppText.js
+++ components/AppText.js
@@ -1,12 +1,6 @@
 import React from "react";
 import {Platform, StyleSheet, Text} from "react-native";
 
-
-
-
-
-//Global use Text..Can be used for Headers or Whatever
-//destructed
 function AppText({ children, style }) {
   return <Text style={[styles.text, style]}>{children}</Text>;
 }
