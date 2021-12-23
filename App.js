09a25fdb35698089c5c9deae19151db368b16f90
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-23 16:29:20 -0600

    3

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
 
-} from "react-native";
-
-import Card from "./components/Card";
 import ListingDetailsScreen from "./screens folder/ListingDetailsScreen";
 
 export default function App() {
