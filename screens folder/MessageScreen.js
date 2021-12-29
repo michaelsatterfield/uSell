790689cf1e9fd90c7a50e22751f2a2eae86e28c9
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-29 16:15:16 -0600

    message screen

--- screens folder/MessageScreen.js
+++ screens folder/MessageScreen.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { FlatList } from 'react-native'
+
+const messages =[
+    {
+        id: 1,
+        title: 'T1',
+        description: 'D1',
+        image: require('../app/assets/mosh.jpg')
+    },
+    {
+        id: 2,
+        title: 'T2',
+        description: 'D2',
+        image: require('../app/assets/mosh.jpg')
+    }
+]
+
 const MessageScreen = () => {
     return (
-   <FlatList data={} renderItem={}/>
+   <FlatList
+       data={messages}
+       keyExtractor={message => message.id.toString()}
+       renderItem={}/>
 
     );
 };
