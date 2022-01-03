9185cc58259eb3979f245439488ec994bea25900
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-03 16:49:50 -0600

    message screen

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import ListingDetailsScreen from "./screens folder/ListingDetailsScreen";
-import ViewImageScreen from "./screens folder/ViewImageScreen";
 import MessageScreen from "./screens folder/MessageScreen";
 
 export default function App() {
